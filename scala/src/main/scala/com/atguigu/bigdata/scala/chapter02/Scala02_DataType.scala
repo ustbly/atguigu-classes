9fd5f761baea0086f49aa0366e875dabad575382
ustbly <ustbly2021@126.com>
2021-01-30 17:47:19 +0800

    scala数据类型

--- scala/src/main/scala/com/atguigu/bigdata/scala/chapter02/Scala02_DataType.scala
+++ scala/src/main/scala/com/atguigu/bigdata/scala/chapter02/Scala02_DataType.scala
@@ -5,6 +5,20 @@ package com.atguigu.bigdata.scala.chapter02
 object Scala02_DataType {
   def main(args: Array[String]): Unit = {
     //Scala是完全面向对象的语言，所以没有基本数据类型
-    var age : Int = 20
+    //Scala中数字也是对象，也可以调用方法
+    val age : Int = 20
+
+    //数据类型分为值类型和引用类型
+    val b : Byte = 10;
+    val s : Short = 10;
+    val i : Int = 10;
+    val c : Char = 'c';
+    val l : Long = 10;
+    val f : Float = 10f;
+    val d : Double = 10;
+    val bln : Boolean = true;
+
+
+    val ii : Integer = 10;
   }
 }
