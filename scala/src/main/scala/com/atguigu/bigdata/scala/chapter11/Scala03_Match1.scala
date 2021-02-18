9038c24cd3ac737e717ab8d75b56422c74c46006
ustbly <ustbly2021@126.com>
2021-02-18 11:10:24 +0800

    2021/2/18 scala模式匹配

---
+++ scala/src/main/scala/com/atguigu/bigdata/scala/chapter11/Scala03_Match1.scala
@@ -0,0 +1,28 @@
+package chapter11
+
+object Scala03_Match1 {
+  def main(args: Array[String]): Unit = {
+    for (ch <- "+-3!") {
+      var sign = 0
+      var digit = 0
+      ch match {
+        case '+' => sign = 1
+        case '-' => sign = -1
+        //条件守卫一般放在最后的case _前面，否则得到的结果可能是不相同的
+        case _ if ch.toString.equals("3") => digit = 3
+        case _ => sign = 2
+      }
+      println(ch + " " + sign + " " + digit)
+    }
+
+    // 如果在case关键字后跟变量名，那么match前表达式的值会赋给那个变量
+    val ch = 'V'
+    ch match {
+      case '+' => println("ok~")
+      case mychar => println("ok~" + mychar)
+      case _ => println("ok~~")
+    }
+
+
+  }
+}
